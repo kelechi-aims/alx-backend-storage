1046d7c647c57d79594890135afcfe5c64041038
kelechi-aims <kelechi.denise@yahoo.com>
2024-01-18 23:58:27 +0000

    task-13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -14,14 +14,14 @@ BEGIN
             SELECT SUM(corrections.score * projects.weight)
 	    FROM corrections
 	    JOIN projects ON corrections.project_id = projects.id
-	    WHERE corrections.user_id = user.id;
+	    WHERE corrections.user_id = users.id;
 	    );
     UPDATE users
         SET total_weight = (
 	    SELECT SUM(projects.weight)
             FROM corrections
             JOIN projects ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user.id;
+            WHERE corrections.user_id = users.id;
             );
     UPDATE users
         SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
