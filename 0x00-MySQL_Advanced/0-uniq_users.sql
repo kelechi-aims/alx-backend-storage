3a95c596fc666b82016fe71e674257b7217cf7b3
kelechi-aims <kelechi.denise@yahoo.com>
2024-01-18 09:50:52 +0000

    task-0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- SQL script that creates a table users w ith these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+-- Creates a table user if not exists
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(225) NOT NULL UNIQUE,
+    name VARCHAR(225)
+    );
