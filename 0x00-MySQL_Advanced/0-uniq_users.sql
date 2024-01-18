b26d6adda6575bedc0317c3682754e00c5765943
kelechi-aims <kelechi.denise@yahoo.com>
2024-01-18 10:19:11 +0000

    task-1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,6 +6,6 @@
 -- Creates a table user if not exists
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(225) NOT NULL UNIQUE,
-    name VARCHAR(225)
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
     );
