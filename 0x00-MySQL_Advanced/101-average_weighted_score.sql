b960af0f4521db95495722f68cb0562eddc56829
kelechi-aims <kelechi.denise@yahoo.com>
2024-01-18 23:41:37 +0000

    task-13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,35 +1,34 @@
--- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
--- Requirements:
--- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
+-- Creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser ()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    ALTER TABLE users ADD total_weighted_score FLOAT NOT NULL;
+    ALTER TABLE users ADD total_weighted_score INT NOT NULL;
     ALTER TABLE users ADD total_weight INT NOT NULL;
 
     UPDATE users
         SET total_weighted_score = (
             SELECT SUM(corrections.score * projects.weight)
-	    FROM corrections
-	    JOIN projects ON corrections.project_id = projects.id
-	    WHERE corrections.user_id = user_id;
-	    );
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+            );
+
     UPDATE users
         SET total_weight = (
-	    SELECT SUM(projects.weight)
-            FROM corrections
-            JOIN projects ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user_id;
+            SELECT SUM(projects.weight)
+                FROM corrections
+                    INNER JOIN projects
+                        ON corrections.project_id = projects.id
+                WHERE corrections.user_id = users.id
             );
+
     UPDATE users
         SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
-
     ALTER TABLE users
         DROP COLUMN total_weighted_score;
     ALTER TABLE users
         DROP COLUMN total_weight;
 END $$
-
 DELIMITER ;
