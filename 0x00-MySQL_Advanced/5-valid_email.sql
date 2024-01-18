c8285ede32309428f3f77ff7ef3c3230be21cd61
kelechi-aims <kelechi.denise@yahoo.com>
2024-01-18 15:45:11 +0000

    task-5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed
-
+DROP TRIGGER IF EXISTS reset_valid_email;
 DELIMTER $$
 
 CREATE TRIGGER reset_valid_email
