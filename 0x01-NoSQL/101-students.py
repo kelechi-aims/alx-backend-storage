e6d9773d9b87dd225a8f60123368c1c209661f86
kelechi-aims <kelechi@aims.edu.gh>
2024-01-23 18:19:36 +0100

    Python function that returns all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,6 +5,12 @@ Python function that returns all students sorted by average score
 
 
 def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+
+    Args:
+        mongo_collection: the pymongo collection object
+    """
     pipeline = [
         {
             "$project": {
