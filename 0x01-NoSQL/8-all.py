8b607a85e7aecffb193b5dd322a16113c8e32479
kelechi-aims <kelechi@aims.edu.gh>
2024-01-23 11:24:31 +0100

    Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+"""
+Python function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in the specified MongoDB collection.
+
+    Args:
+        mongo_collection: pymongo collection object.
+
+    Returns:
+        A list of documents in the collection.
+    """
+    return list(mongo_collection.find())
